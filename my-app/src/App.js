9f40cfb350252115b23801afc02891495f61ee2c
Victoriia Ulynets <123025408+Victoriia88@users.noreply.github.com>
2023-11-29 15:29:09 +0200

    in procces

---
+++ my-app/src/App.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countdownDate = new Date("March 11, 2024 00:00:00").getTime();
+
+    interval.current = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        //stop
+        clearInterval(interval.current);
+      } else {
+        //update
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+  return (
+    <section className="timer-container">
+      <section className="timer">
+        <div>
+          <span className="mdi mdi-calendar-clock timer-icon"></span>
+          <h2>COUNTDOWN TIMER</h2>
+          <p>
+            Countdown to a really special date. One you could or would never
+            imagine!
+          </p>
+        </div>
+        <div className="timer-date">
+          <section>
+            <p>{timerDays}</p>
+            <p>
+              <small>Days</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerHours}</p>
+            <p>
+              <small>Hours</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerMinutes}</p>
+            <p>
+              <small>Minutes</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerSeconds}</p>
+            <p>
+              <small>Seconds</small>
+            </p>
+          </section>
+        </div>
+        <div>
+          <button>Start Timer</button>
+          <button>Stop Timer</button>
+          <button>Reset Timer</button>
+        </div>
+      </section>
+    </section>
+  );
+};
+export default App;
